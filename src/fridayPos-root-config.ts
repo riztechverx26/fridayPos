268e3ed36d5373302c387a595aaa404bee31908c
alihasnain-cd <ali.hasnain@codedistrict.com>
2021-07-27 17:00:35 +0500

    app registered

--- src/fridayPos-root-config.ts
+++ src/fridayPos-root-config.ts
@@ -1,19 +1,19 @@
 import { registerApplication, start } from "single-spa";
 
-registerApplication({
-  name: "@single-spa/welcome",
-  app: () =>
-    System.import(
-      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
-    ),
-  activeWhen: ["/"],
-});
-
 // registerApplication({
-//   name: "@fridayPos/navbar",
-//   app: () => System.import("@fridayPos/navbar"),
-//   activeWhen: ["/"]
+//   name: "@single-spa/welcome",
+//   app: () =>
+//     System.import(
+//       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+//     ),
+//   activeWhen: ["/"],
 // });
+
+registerApplication({
+  name: "@friday/mf-retail",
+  app: () => System.import("@friday/mf-retail"),
+  activeWhen: ["/"]
+});
 
 start({
   urlRerouteOnly: true,
